95d2f453f3a669ffa6d402d7ce3218120997751b
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-07-12 19:28:49 +0530

    -

---
+++ server.js
@@ -0,0 +1,13 @@
+Bun.serve({
+  fetch(req) {
+   const url = new URL(req.url);
+    if (url.pathname.endsWith("/") || url.pathname.endsWith("/index.html")){
+
+      console.log("Serving index.html");
+      return new Response(Bun.file(import.meta.dir + "/index.html"));
+    }
+    
+     // all other routes
+     return new Response("Hello!");
+  }
+});
